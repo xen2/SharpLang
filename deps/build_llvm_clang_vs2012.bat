1660fefe93e14c4974f7aa8516653412bb6f9ee5
Virgile Bello <virgile.bello@gmail.com>
2014-06-21 22:08:48 +0900

    Added CppBackend target to LLVM

--- deps/build_llvm_clang_vs2012.bat
+++ deps/build_llvm_clang_vs2012.bat
@@ -2,7 +2,7 @@ call "%VS110COMNTOOLS%..\..\VC\vcvarsall.bat" x86
 
 mkdir llvm\build
 pushd llvm\build
-cmake .. -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=..\..\clang -DCLANG_INCLUDE_TESTS=OFF
+cmake .. -DLLVM_TARGETS_TO_BUILD=X86;CppBackend -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=..\..\clang -DCLANG_INCLUDE_TESTS=OFF
 msbuild LLVM.sln /p:Configuration=RelWithDebInfo
 msbuild LLVM.sln /p:Configuration=Debug
 popd
