3e455c906aa5546bfd37d3a7c2eff2ef9555002d
Virgile Bello <virgile.bello@gmail.com>
2014-06-26 01:58:50 +0900

    Test for methods on struct types.

--- src/SharpLang.Compiler.Tests/tests/SimpleStruct.cs
+++ src/SharpLang.Compiler.Tests/tests/SimpleStruct.cs
@@ -4,6 +4,8 @@ public static class Program
     {
         public string A;
         public int B;
+
+        public int C { get; set; }
     }
 
     public static void Main()
@@ -11,8 +13,10 @@ public static class Program
         var test = new Test();
         test.A = "Test";
         test.B = 32;
+        test.C = 64;
 
         System.Console.WriteLine(test.A);
         System.Console.WriteLine(test.B);
+        System.Console.WriteLine(test.C);
     }
 }
