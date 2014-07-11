0ecdfecec3e63bbdf5ae70ee8fda37b97c31f944
Virgile Bello <virgile.bello@gmail.com>
2014-07-12 02:31:18 +0900

    Added test for Castclass.

---
+++ src/SharpLang.Compiler.Tests/tests/SimpleClassCast.cs
@@ -0,0 +1,57 @@
+public static class Program
+{
+    public class Test1
+    {
+        public virtual string A()
+        {
+            return "Test1";
+        }
+    }
+
+    public interface ITest
+    {
+    }
+
+    public class Test2 : Test1, ITest
+    {
+        public override string A()
+        {
+            return "Test2";
+        }
+    }
+
+    class Exception1 : System.Exception
+    {
+    }
+    
+    public static void Main()
+    {
+        object test1 = new Test1();
+        object test2 = new Test2();
+
+        // Test valid casts
+        System.Console.WriteLine((Test1)test1 != null);
+        System.Console.WriteLine((Test1)test2 != null);
+        System.Console.WriteLine((Test2)test2 != null);
+        System.Console.WriteLine((ITest)test2 != null);
+
+        // TODO: Should work as soon as we emit InvalidCastException locally for tests
+        //try
+        //{
+        //    System.Console.WriteLine((Test2)test1 != null);
+        //}
+        //catch (System.InvalidCastException e)
+        //{
+        //    System.Console.WriteLine("InvalidCast");
+        //}
+        //
+        //try
+        //{
+        //    System.Console.WriteLine((ITest)test1 != null);
+        //}
+        //catch (System.InvalidCastException e)
+        //{
+        //    System.Console.WriteLine("InvalidCast");
+        //}
+    }
+}
