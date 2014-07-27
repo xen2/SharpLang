58c40c62e066bf43ca8a76cb7b852381e2f1179e
Virgile Bello <virgile.bello@gmail.com>
2014-07-27 22:21:29 +0200

    ValueRef.Empty and TypeRef.Empty (equivalent to NULL) have been added.

--- src/SharpLLVM/Additional/TypeRef.cs
+++ src/SharpLLVM/Additional/TypeRef.cs
@@ -2,6 +2,8 @@
 {
     public partial struct TypeRef
     {
+        public static readonly TypeRef Empty = new TypeRef();
+
         public override string ToString()
         {
             return LLVM.PrintTypeToString(this);
--- src/SharpLLVM/Additional/ValueRef.cs
+++ src/SharpLLVM/Additional/ValueRef.cs
@@ -2,6 +2,8 @@
 {
     public partial struct ValueRef
     {
+        public static readonly ValueRef Empty = new ValueRef();
+
         public override string ToString()
         {
             return LLVM.PrintValueToString(this);
