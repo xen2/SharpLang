c3d9616351a053b602c676b78f648009d32d1d08
Virgile Bello <virgile.bello@gmail.com>
2014-08-02 15:47:32 +0900

    Squashed 'src/mcs/' content from commit 8f75520
    git-subtree-dir: src/mcs
    git-subtree-split: 8f75520f31dc35053c7ccf20c502e6a3b9d810bf

---
+++ class/Mono.Debugger.Soft/Mono.Debugger.Soft/InvokeOptions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Mono.Debugger.Soft
+{
+	[Flags]
+	public enum InvokeOptions {
+		None = 0,
+		/*
+		 * Disable breakpoints on the thread doing the invoke
+		 */
+		DisableBreakpoints = 1,
+		/*
+		 * Only resume the target thread during the invoke
+		 */
+		SingleThreaded = 2,
+		/*
+		 * Return the changed receiver when invoking
+		 * a valuetype method.
+		 */
+		ReturnOutThis = 4,
+		/*
+		 * Return the values of out arguments
+		 */
+		ReturnOutArgs = 8
+	}
+}
