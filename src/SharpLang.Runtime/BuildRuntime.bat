488c4f20a29d0f741fcda205659209d561870e4a
Virgile Bello <virgile.bello@gmail.com>
2014-09-06 18:22:37 +0900

    Runtime is now properly built using i686-pc-mingw32 as well.

--- src/SharpLang.Runtime/BuildRuntime.bat
+++ src/SharpLang.Runtime/BuildRuntime.bat
@@ -1,4 +1,3 @@
-..\..\deps\llvm\build\RelWithDebInfo\bin\clang RuntimeType.cpp -emit-llvm -c -o RuntimeType.bc -O3 -I../../deps/llvm/build/include -I../../deps/llvm/include
-..\..\deps\llvm\build\RelWithDebInfo\bin\clang Exception.cpp -emit-llvm -c -o Exception.bc -O3 -I../../deps/llvm/build/include -I../../deps/llvm/include
+..\..\deps\llvm\build\RelWithDebInfo\bin\clang RuntimeType.cpp --target=i686-pc-mingw32 -std=c++11 -emit-llvm -c -o RuntimeType.bc -O3 -I../../deps/llvm/build/include -I../../deps/llvm/include -I../../deps/mingw32/i686-w64-mingw32/include -I../../deps/mingw32/i686-w64-mingw32/include/c++ -I../../deps/mingw32/i686-w64-mingw32/include/c++/i686-w64-mingw32 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
+..\..\deps\llvm\build\RelWithDebInfo\bin\clang Exception.cpp --target=i686-pc-mingw32 -std=c++11 -emit-llvm -c -o Exception.bc -O3 -I../../deps/llvm/build/include -I../../deps/llvm/include -I../../deps/mingw32/i686-w64-mingw32/include -I../../deps/mingw32/i686-w64-mingw32/include/c++ -I../../deps/mingw32/i686-w64-mingw32/include/c++/i686-w64-mingw32 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
 ..\..\deps\llvm\build\RelWithDebInfo\bin\llvm-link RuntimeType.bc Exception.bc -o Runtime.bc
-..\..\deps\llvm\build\RelWithDebInfo\bin\llc -march=cpp Runtime.bc -cppgen=contents -o RuntimeInline.inc
