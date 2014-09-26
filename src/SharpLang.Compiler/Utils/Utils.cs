7ff3013ba034b987b41b5b248594b602dfd5d942
Virgile Bello <virgile.bello@gmail.com>
2014-09-27 00:56:38 +0900

    Tests: Added support for compiling .il files through ilasm.exe.

--- src/SharpLang.Compiler/Utils/Utils.cs
+++ src/SharpLang.Compiler/Utils/Utils.cs
@@ -1,10 +1,54 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace SharpLang.Compiler.Utils
 {
     public class Utils
     {
+        /// <summary>
+        /// Executes process and capture its output.
+        /// </summary>
+        /// <param name="processStartInfo">The process start information.</param>
+        /// <param name="output">The output.</param>
+        /// <returns></returns>
+        public static Process ExecuteAndCaptureOutput(ProcessStartInfo processStartInfo, out string output)
+        {
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+
+            var process = new Process { StartInfo = processStartInfo };
+
+            var outputBuilder = new StringBuilder();
+
+            process.OutputDataReceived += (sender, args) =>
+            {
+                lock (outputBuilder)
+                {
+                    if (args.Data != null)
+                        outputBuilder.AppendLine(args.Data);
+                }
+            };
+            process.ErrorDataReceived += (sender, args) =>
+            {
+                lock (outputBuilder)
+                {
+                    if (args.Data != null)
+                        outputBuilder.AppendLine(args.Data);
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            output = outputBuilder.ToString();
+            return process;
+        }
+
         public static string GetTestsDirectory()
         {
             var directory = Directory.GetParent(Directory.GetCurrentDirectory());
