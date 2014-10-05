03074219761b42dea1cb985d1ebf04d75f1a8ab2
Virgile Bello <virgile.bello@gmail.com>
2014-10-05 16:43:38 +0900

    Compiler: Interfaces now have their own RTTI (null) global instead of System.Object one, so that is/as properly works.

--- src/SharpLang.Compiler.Tests/tests/SimpleClassIs.cs
+++ src/SharpLang.Compiler.Tests/tests/SimpleClassIs.cs
@@ -12,6 +12,10 @@ public static class Program
     {
     }
 
+    public interface ITest2
+    {
+    }
+
     public class Test2 : Test1, ITest
     {
         public override string A()
@@ -28,15 +32,19 @@ public static class Program
         System.Console.WriteLine(test1 is Test1 ? "True" : "False");
         System.Console.WriteLine(test1 is Test2);
         System.Console.WriteLine(test1 is ITest);
+        System.Console.WriteLine(test1 is ITest2);
         System.Console.WriteLine(test2 is Test1);
         System.Console.WriteLine(test2 is Test2);
         System.Console.WriteLine(test2 is ITest);
+        System.Console.WriteLine(test2 is ITest2);
 
         System.Console.WriteLine(test1 as Test1 != null ? "True" : "False");
         System.Console.WriteLine(test1 as Test2 != null);
         System.Console.WriteLine(test1 as ITest != null);
+        System.Console.WriteLine(test1 as ITest2 != null);
         System.Console.WriteLine(test2 as Test1 != null);
         System.Console.WriteLine(test2 as Test2 != null);
         System.Console.WriteLine(test2 as ITest != null);
+        System.Console.WriteLine(test2 as ITest2 != null);
     }
 }
