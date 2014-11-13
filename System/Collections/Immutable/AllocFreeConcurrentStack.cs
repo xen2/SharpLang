24f7f426dc27c333e0657a421e8c574227dd8d40
Virgile Bello <virgile.bello@gmail.com>
2014-11-14 00:57:17 +0900

    Squashed 'src/corefx/System.Collections.Immutable/' content from commit ec7c8de
    git-subtree-dir: src/corefx/System.Collections.Immutable
    git-subtree-split: ec7c8de32f81a4cb5fe71cba7fd0778fa778e3f3

---
+++ System/Collections/Immutable/AllocFreeConcurrentStack.cs
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace System.Collections.Immutable
+{
+    [DebuggerDisplay("Count = {stack != null ? stack.Count : 0}")]
+    internal static class AllocFreeConcurrentStack<T>
+    {
+        private const int MaxSize = 35;
+
+        [ThreadStatic]
+        private static Stack<RefAsValueType<T>> stack;
+
+        public static void TryAdd(T item)
+        {
+            Stack<RefAsValueType<T>> localStack = stack; // cache in a local to avoid unnecessary TLS hits on repeated accesses
+            if (localStack == null)
+            {
+                stack = localStack = new Stack<RefAsValueType<T>>(MaxSize);
+            }
+
+            // Just in case we're in a scenario where an object is continually requested on one thread
+            // and returned on another, avoid unbounded growth of the stack.
+            if (localStack.Count < MaxSize)
+            {
+                localStack.Push(new RefAsValueType<T>(item));
+            }
+        }
+
+        public static bool TryTake(out T item)
+        {
+            Stack<RefAsValueType<T>> localStack = stack; // cache in a local to avoid unnecessary TLS hits on repeated accesses
+            if (localStack != null && localStack.Count > 0)
+            {
+                item = localStack.Pop().Value;
+                return true;
+            }
+
+            item = default(T);
+            return false;
+        }
+    }
+}
---
+++ System/Collections/Immutable/SecureObjectPool.cs
@@ -0,0 +1,141 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Validation;
+
+namespace System.Collections.Immutable
+{
+    /// <summary>
+    /// Object pooling utilities.
+    /// </summary>
+    internal class SecureObjectPool
+    {
+        /// <summary>
+        /// The ever-incrementing (and wrap-on-overflow) integer for owner id's.
+        /// </summary>
+        private static int poolUserIdCounter;
+
+        /// <summary>
+        /// The ID reserved for unassigned objects.
+        /// </summary>
+        internal const int UnassignedId = -1;
+
+        /// <summary>
+        /// Returns a new ID.
+        /// </summary>
+        internal static int NewId()
+        {
+            int result;
+            do
+            {
+                result = Interlocked.Increment(ref poolUserIdCounter);
+            }
+            while (result == UnassignedId);
+
+            return result;
+        }
+    }
+
+    internal class SecureObjectPool<T, TCaller>
+        where TCaller : ISecurePooledObjectUser
+    {
+        public void TryAdd(TCaller caller, SecurePooledObject<T> item)
+        {
+            // Only allow the caller to recycle this object if it is the current owner.
+            if (caller.PoolUserId == item.Owner)
+            {
+                item.Owner = SecureObjectPool.UnassignedId;
+                AllocFreeConcurrentStack<SecurePooledObject<T>>.TryAdd(item);
+            }
+        }
+
+        public bool TryTake(TCaller caller, out SecurePooledObject<T> item)
+        {
+            if (caller.PoolUserId != SecureObjectPool.UnassignedId && AllocFreeConcurrentStack<SecurePooledObject<T>>.TryTake(out item))
+            {
+                item.Owner = caller.PoolUserId;
+                return true;
+            }
+            else
+            {
+                item = null;
+                return false;
+            }
+        }
+
+        public SecurePooledObject<T> PrepNew(TCaller caller, T newValue)
+        {
+            Requires.NotNullAllowStructs(newValue, "newValue");
+            var pooledObject = new SecurePooledObject<T>(newValue);
+            pooledObject.Owner = caller.PoolUserId;
+            return pooledObject;
+        }
+    }
+
+    internal interface ISecurePooledObjectUser
+    {
+        int PoolUserId { get; }
+    }
+
+    internal class SecurePooledObject<T>
+    {
+        private readonly T value;
+        private int owner;
+
+        internal SecurePooledObject(T newValue)
+        {
+            Requires.NotNullAllowStructs(newValue, "newValue");
+            this.value = newValue;
+        }
+
+        /// <summary>
+        /// Gets or sets the current owner of this recyclable object.
+        /// </summary>
+        internal int Owner
+        {
+            get { return this.owner; }
+            set { this.owner = value; }
+        }
+
+        internal SecurePooledObjectUser Use<TCaller>(TCaller caller)
+            where TCaller : ISecurePooledObjectUser
+        {
+            this.ThrowDisposedIfNotOwned(caller);
+            return new SecurePooledObjectUser(this);
+        }
+
+        internal void ThrowDisposedIfNotOwned<TCaller>(TCaller caller)
+            where TCaller : ISecurePooledObjectUser
+        {
+            if (caller.PoolUserId != this.owner)
+            {
+                throw new ObjectDisposedException(caller.GetType().FullName);
+            }
+        }
+
+        internal struct SecurePooledObjectUser : IDisposable
+        {
+            private readonly SecurePooledObject<T> value;
+
+            internal SecurePooledObjectUser(SecurePooledObject<T> value)
+            {
+                this.value = value;
+            }
+
+            internal T Value
+            {
+                get { return this.value.value; }
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
