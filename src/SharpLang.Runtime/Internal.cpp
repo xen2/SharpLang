6392ae88f90f0ac0f4630d754f49f27d6711e179
Virgile Bello <virgile.bello@gmail.com>
2015-01-07 00:36:11 +0900

    [Runtime] File.Exists: Added implementation for MonoIO.GetFileAttributes(), used by File.Exists().

--- src/SharpLang.Runtime/Internal.cpp
+++ src/SharpLang.Runtime/Internal.cpp
@@ -392,6 +392,20 @@ extern "C" void* System_IO_MonoIO__get_ConsoleError__()
 	return GetStdHandle(STD_ERROR_HANDLE);
 }
 
+extern "C" int32_t System_IO_MonoIO__GetFileAttributes_System_String_System_IO_MonoIOError__(String* path, int32_t* error)
+{
+	*error = ERROR_SUCCESS;
+
+	auto result = GetFileAttributesW((LPCWSTR)path->value);
+
+	if (result == -1)
+		*error = GetLastError();
+
+	// TODO: It seems FindFirstFile might be necessary in case file is already opened
+
+	return result;
+}
+
 extern "C" int32_t System_IO_MonoIO__Write_System_IntPtr_System_Byte___System_Int32_System_Int32_System_IO_MonoIOError__(void* handle, Array<uint8_t>* src, int32_t src_offset, int32_t count, int32_t* error)
 {
 	*error = ERROR_SUCCESS;
