dd4fbdcacd1bcc957ac53618bca4b1f0d69523a9
Virgile Bello <virgile.bello@gmail.com>
2015-01-18 01:34:50 +0900

    [corlib] Moved SharpLang partial Type implementation directly in Type.cs (it was triggering a mcs bug).

--- src/mcs/class/corlib/System/Type.cs
+++ src/mcs/class/corlib/System/Type.cs
@@ -1767,5 +1767,36 @@ namespace System {
 			throw new NotImplementedException ();
 		}
 #endif
+
+#if SHARPLANG_RUNTIME
+        private static Type internal_from_name(string name, bool throwOnError, bool ignoreCase)
+        {
+            string fullname, assemblyName;
+
+            SplitAssemblyQualifiedName(name, out fullname, out assemblyName);
+
+            throw new NotImplementedException();
+        }
+
+        private static void SplitAssemblyQualifiedName(string assemblyQualifiedName, out string fullname, out string assemblyName)
+        {
+            var assemblyNameStartIndex = assemblyQualifiedName.LastIndexOf(',');
+
+            if (assemblyNameStartIndex != -1)
+            {
+                fullname = assemblyQualifiedName.Substring(0, assemblyNameStartIndex++);
+
+                while (assemblyQualifiedName[assemblyNameStartIndex] == ' ')
+                    assemblyNameStartIndex++;
+
+                assemblyName = fullname.Substring(assemblyNameStartIndex);
+            }
+            else
+            {
+                fullname = assemblyQualifiedName;
+                assemblyName = string.Empty;
+            }
+        }
+#endif
 	}
 }
