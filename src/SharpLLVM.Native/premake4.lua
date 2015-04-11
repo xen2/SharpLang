5a7e8143cbab611b4e920938316f283fa7fd615d
Virgile Bello <virgile.bello@gmail.com>
2015-04-11 20:22:57 +0900

    [Build] Only generate SWIG binding if swig is available.

--- src/SharpLLVM.Native/premake4.lua
+++ src/SharpLLVM.Native/premake4.lua
@@ -1,26 +1,31 @@
 function SetupSWIGBindings()
   local c = configuration '**/LLVM.i'
   
-    -- A message to display while this build step is running (optional)
-    buildmessage 'Generating bindings with SWIG for: %{file.name}'
-
     -- One or more commands to run (required)
     local prj = premake.api.scope.project.location
-    local gen = path.getrelative(prj, path.getabsolute(gendir))
-    local current = path.getrelative(prj, path.getabsolute("."))
-    local out = path.join(current, '%{file.basename}_wrap.cpp')
-    local outdir = path.getrelative(prj, path.getabsolute("../SharpLLVM"))
-    local llvm = path.getrelative(prj, path.getabsolute(path.join(LLVMRootDir, "include")))
-    local swig = path.getrelative(prj, path.getabsolute(path.join(toolsdir, 'swig/swig')))
+	
+	-- Check swig existence before actually adding this command (otherwise we generate a project file which doesn't generate wrapper
+    if os.isdir(path.getabsolute(path.join(toolsdir, 'swig'))) then
+      -- A message to display while this build step is running (optional)
+      buildmessage 'Generating bindings with SWIG for: %{file.name}'
 
-    local cmd = '"' .. swig .. '" -w302 -csharp -I' .. llvm .. " -I" .. current
-      .. " -namespace SharpLLVM -dllimport SharpLLVM.Native.dll"
-      .. " -outdir " .. outdir .. " -o " .. out .. " %{file.relpath}"
+      local gen = path.getrelative(prj, path.getabsolute(gendir))
+      local current = path.getrelative(prj, path.getabsolute("."))
+      local out = path.join(current, '%{file.basename}_wrap.cpp')
+      local outdir = path.getrelative(prj, path.getabsolute("../SharpLLVM"))
+      local llvm = path.getrelative(prj, path.getabsolute(path.join(LLVMRootDir, "include")))
+      local swig = path.getrelative(prj, path.getabsolute(path.join(toolsdir, 'swig/swig')))
 
-    buildcommands { cmd }
+      local cmd = '"' .. swig .. '" -w302 -csharp -I' .. llvm .. " -I" .. current
+        .. " -namespace SharpLLVM -dllimport SharpLLVM.Native.dll"
+        .. " -outdir " .. outdir .. " -o " .. out .. " %{file.relpath}"
 
-    -- One or more outputs resulting from the build (required)
-    buildoutputs { '%{file.basename}_wrap.cpp' }
+      buildcommands { cmd }
+
+      -- One or more outputs resulting from the build (required)
+      buildoutputs { '%{file.basename}_wrap.cpp' }
+    end
+
 
   configuration(c)
 end
